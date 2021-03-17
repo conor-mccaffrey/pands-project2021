636b52f7d8655d2074152dc82611000547a22ba5
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-03-17 15:48:44 +0000

    added dataset to repository

---
+++ uploadDataSet.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Upload our dataset and add some structure to it
+names = ['SepalLength','SepalWidth','PetalLength','PetalWidth','Class'] # from researching the dataset, I found what each column of data represents and wanted to reflect this in the dataset
+data = pd.read_csv('iris.data', names = names)  # reading in the data file and applying our column names
+print(data.head())
