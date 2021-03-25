3e5b8d948b408db706f2d776972426cf0b7858eb
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-03-25 22:10:06 +0000

    setting up scatterPlot program

---
+++ scatterPlot.py
@@ -0,0 +1,14 @@
+# Program to output scatter plots of each pair of variables
+
+import numpy as np
+import pandas as pd
+import sys
+import matplotlib as mpl
+import os
+import matplotlib.pyplot as plt
+from PIL import Image # this lets us convert images into arrays
+import matplotlib.patches as mpatches # needed for waffle charts
+import seaborn as sns
+
+
+from uploadDataSet import data # importing our data. Again, this wont be necessary for final program
