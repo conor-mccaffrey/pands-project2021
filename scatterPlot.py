8711cf8eb78c6de5f01727a6532270473d0b45b3
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-03-26 14:53:03 +0000

    updated code and titles for scatter plots

--- scatterPlot.py
+++ scatterPlot.py
@@ -15,25 +15,43 @@ from uploadDataSet import data # importing our data. Again, this wont be necessa
 plt.figure() # idea adapted from reference StackOverflow (reference 19) 
 sns.set_style("darkgrid") # adapted from StackOverflow (reference 20)
 sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['SepalWidth_cm'], hue = data['Class']) #setting properties of plot (reference 20)
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.suptitle('Sepal Length v Sepal Width', fontsize = 12) 
 plt.savefig('scatPlotSL_v_SW.png') # saving output to a new file
 
 plt.figure()
 sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['PetalWidth_cm'], hue = data['Class']) 
+plt.xlabel('Sepal Length')
+plt.ylabel('Petal Width')
+plt.suptitle('Sepal Length v Petal Width', fontsize = 12) 
 plt.savefig('scatPlotSL_v_PW.png')
 
 plt.figure()
 sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['PetalLength_cm'],hue = data['Class']) 
+plt.xlabel('Sepal Length')
+plt.ylabel('Petal Length')
+plt.suptitle('Sepal Length v Petal Length', fontsize = 12) 
 plt.savefig('scatPlotSL_v_PL.png')
 
 plt.figure()
 sns.scatterplot(data=data, x=data['PetalLength_cm'], y=data['PetalWidth_cm'], hue = data['Class'])
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+plt.suptitle('Petal Length v Petal Width', fontsize = 12) 
 plt.savefig('scatPlotPL_v_PW.png')
 
 plt.figure()
 sns.scatterplot(data=data, x=data['PetalLength_cm'], y=data['SepalWidth_cm'], hue = data['Class']) 
+plt.xlabel('Petal Length')
+plt.ylabel('Sepal Width')
+plt.suptitle('Petal Length v Sepal Width', fontsize = 12) 
 plt.savefig('scatPlotPL_v_SW.png')
 
 plt.figure()
 sns.scatterplot(data=data, x=data['SepalWidth_cm'], y=data['PetalWidth_cm'], hue = data['Class']) 
+plt.xlabel('Sepal Width')
+plt.ylabel('Petal Width')
+plt.suptitle('Sepal Width v Petal Width', fontsize = 12) 
 plt.savefig('scatPlotSW_v_PW.png')
 
