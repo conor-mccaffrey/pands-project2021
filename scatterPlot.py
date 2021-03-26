0b09284bcd71a5d3ef482849345c6181d4ce776c
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-03-26 14:16:41 +0000

    code complete for scatterplots

--- scatterPlot.py
+++ scatterPlot.py
@@ -12,29 +12,28 @@ import seaborn as sns
 
 
 from uploadDataSet import data # importing our data. Again, this wont be necessary for final program
+plt.figure() # idea adapted from reference StackOverflow (reference 19) 
+sns.set_style("darkgrid") # adapted from StackOverflow (reference 20)
+sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['SepalWidth_cm'], hue = data['Class']) #setting properties of plot (reference 20)
+plt.savefig('scatPlotSL_v_SW.png') # saving output to a new file
 
-sns.set_style("darkgrid") # adapted from StackOverflow (reference 19)
-f =sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['SepalWidth_cm'], hue = data['Class']) #setting properties of plot (reference 20)
-plt.savefig('scatPlotSL_v_SW.png')
-
-
-g =sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['PetalWidth_cm'], hue = data['Class']) 
+plt.figure()
+sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['PetalWidth_cm'], hue = data['Class']) 
 plt.savefig('scatPlotSL_v_PW.png')
 
- 
-h=sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['PetalLength_cm'],hue = data['Class']) 
+plt.figure()
+sns.scatterplot(data=data, x=data['SepalLength_cm'], y=data['PetalLength_cm'],hue = data['Class']) 
 plt.savefig('scatPlotSL_v_PL.png')
 
-
-i = sns.scatterplot(data=data, x=data['PetalLength_cm'], y=data['PetalWidth_cm'], hue = data['Class']) 
+plt.figure()
+sns.scatterplot(data=data, x=data['PetalLength_cm'], y=data['PetalWidth_cm'], hue = data['Class'])
 plt.savefig('scatPlotPL_v_PW.png')
 
-
-l = sns.scatterplot(data=data, x=data['PetalLength_cm'], y=data['SepalWidth_cm'], hue = data['Class']) 
-plt.legend()
+plt.figure()
+sns.scatterplot(data=data, x=data['PetalLength_cm'], y=data['SepalWidth_cm'], hue = data['Class']) 
 plt.savefig('scatPlotPL_v_SW.png')
 
-
-a=sns.scatterplot(data=data, x=data['SepalWidth_cm'], y=data['PetalWidth_cm'], hue = data['Class']) 
+plt.figure()
+sns.scatterplot(data=data, x=data['SepalWidth_cm'], y=data['PetalWidth_cm'], hue = data['Class']) 
 plt.savefig('scatPlotSW_v_PW.png')
 
