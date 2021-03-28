e95bc87236fd1d447f79014d4d9c449e8fa0af7e
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-03-28 21:43:03 +0100

    updated comments

--- uploadDataSet.py
+++ uploadDataSet.py
@@ -9,7 +9,7 @@ import matplotlib.patches as mpatches # needed for waffle charts
 
 
 # Upload our dataset and add some structure to it. I've wrapped this in a try/except block to demonstrate knowledge and also to 
-# confirm to user the upload was successful
+# confirm to the user the upload was successful
 try:
     data = pd.read_csv('iris.data', header=None)  # reading in the data file 
     data.columns = ['SepalLength_cm','SepalWidth_cm','PetalLength_cm','PetalWidth_cm','Class'] # adding column names to files
@@ -17,5 +17,3 @@ except BaseException as Exception:
     print('We have hit a snag, try that again')
 else:
     print('Upload of dataset successful') # to confirm that we have uploaded the dataset without any issues.
-
-print(data)
