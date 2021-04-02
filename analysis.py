95894fd8da34fd4407dc5dbe85de1956851c68fa
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-04-02 18:40:16 +0100

    ran analysis program to test it

--- analysis.py
+++ analysis.py
@@ -11,6 +11,7 @@ import matplotlib as mpl
 import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches # needed for waffle charts
+import seaborn as sns
 
 # Upload our dataset and add some structure to it. I've wrapped this in a try/except block to demonstrate knowledge and also to 
 # confirm to the user the upload was successful
@@ -40,3 +41,40 @@ with open('variablesSummary.txt', 'w') as text_file: # creating a new .txt file 
 
 sys.stdout = original_stdout # Reset the standard output to its original value to avoid adding data we dont want to newly created file
 
+sns.displot(data['SepalLength_cm'], kde= False, bins = 15, color = 'green', edgecolor = 'orange', alpha=0.6) # setting kde as False in order to remove the density line as it is not relavant to task
+# also altered alpha value to change transparency of the graph (adapted from Reference 13)
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Frequency')
+plt.suptitle('Sepal Length', fontsize = 12) # adding main title to histogram and setting fontsize
+plt.ylim(0,25) # setting y-axis limits
+plt.xlim(3,10) # setting x-axis limits
+plt.grid(ls = '--', lw = '0.1', color = '#BEBEBE', axis = 'y') # modifying grid lines so that they dont 'overpower' the data and used hex value for colour
+plt.savefig('sepalLengthHist.png') # saving output to specified .png file
+
+sns.displot(data['SepalWidth_cm'], kde= False, bins = 15, color = 'yellow', edgecolor = 'orange', alpha=0.6)
+plt.xlabel('Sepal Width (cm)')
+plt.ylabel('Frequency')
+plt.suptitle('Sepal Width', fontsize = 12) 
+plt.ylim(0,50) 
+plt.xlim(0,6) 
+plt.grid(ls = '--', lw = '0.1', color = '#BEBEBE', axis = 'y') 
+plt.savefig('sepalWidthHist.png') 
+
+sns.displot(data['PetalLength_cm'], kde= False, bins = 15, color = 'blue', edgecolor = 'orange', alpha=0.6)
+plt.xlabel('Petal Length (cm)')
+plt.ylabel('Frequency')
+plt.suptitle('Petal Length', fontsize = 12) 
+plt.ylim(0,50) 
+plt.xlim(0,10) 
+plt.grid(ls = '--', lw = '0.1', color = '#BEBEBE', axis = 'y') 
+plt.savefig('petalLengthHist.png') 
+
+
+sns.displot(data['PetalWidth_cm'], kde= False, bins = 15, color = 'purple', edgecolor = 'orange', alpha=0.6)
+plt.xlabel('Petal Width (cm)')
+plt.ylabel('Frequency')
+plt.suptitle('Petal Width', fontsize = 12) 
+plt.ylim(0,40) 
+plt.xlim(0,4) 
+plt.grid(ls = '--', lw = '0.1', color = '#BEBEBE', axis = 'y') 
+plt.savefig('petalWidthHist.png') 
