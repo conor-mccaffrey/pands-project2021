b4e332d46d6caeb01796f4d2893c04405f45d548
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-04-02 18:25:03 +0100

    set up intro to analysis program

---
+++ analysis.py
@@ -0,0 +1,6 @@
+# Final program that outputs a summary of each variable to a single text file,
+# saves a histogram of each variable to png files, 
+# and outputs a scatter plot of each pair of variables.
+
+# Author: Conor McCaffrey
+
