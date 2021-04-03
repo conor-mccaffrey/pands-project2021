0238288756f573b9976a1ca277ab8b19966a3f5b
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-04-03 14:17:10 +0100

    uploaded 3D work to README

---
+++ scatterPlot_3D.py
@@ -0,0 +1,10 @@
+# 3D Scatter Plots
+
+import plotly.express as px
+from uploadDataSet import data
+
+
+fig = px.scatter_3d(data, x='SepalLength_cm', y='SepalWidth_cm', z='PetalWidth_cm',
+                    color='PetalLength_cm', symbol='Class') # compiled from reference 28
+fig.update_layout(legend=dict(yanchor="top", y=0.90, xanchor="left",x=0.2)) # positioning the legend so it is separate from the color bar chart (reference 29)
+fig.write_image('variablesPlot3D.png', width=1800, height=900) # setting height and width variables 
